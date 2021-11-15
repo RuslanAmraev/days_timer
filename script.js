a48a3fb7d89816e7577fb7628669ddd565381aa6
RuslanAmraev <beardedsmellyfox@gmail.com>
2021-11-15 19:30:13 +0600

    1

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const theBeginningOfaRelationship = new Date ('2021, 10, 26 00:30:00');
 
 function setDate(){
     let today = new Date ();
-    let days = Math.floor((today.getTime() - theBeginningOfaRelationship.getTime())/(1000*60*60*24));
-    let hours = Math.floor((today.getTime() - theBeginningOfaRelationship.getTime())/(1000*60*60));
-    let minutes = Math.floor((today.getTime() - theBeginningOfaRelationship.getTime())/(1000*60));
-    let seconds = Math.floor((today.getTime() - theBeginningOfaRelationship.getTime())/(1000));
+    let days = Number(Math.floor((today.getTime() - theBeginningOfaRelationship.getTime())/(1000*60*60*24)));
+    let hours = Number(Math.floor((today.getTime() - theBeginningOfaRelationship.getTime())/(1000*60*60)));
+    let minutes = Number(Math.floor((today.getTime() - theBeginningOfaRelationship.getTime())/(1000*60)));
+    let seconds = Number(Math.floor((today.getTime() - theBeginningOfaRelationship.getTime())/(1000)));
     box.innerHTML = (Number(days)) + ' дней ' + (Number(hours-(days*24))) + ' часов ' + (Number(minutes-(hours*60))) + ' минут ' + (Number(seconds-(minutes * 60))) + ' секунд'
 }
 
